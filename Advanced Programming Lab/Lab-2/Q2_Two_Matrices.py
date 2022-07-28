c8a7764ed23252456f8add05bbcb3f7ef78cc8db
Aditya Chandra <74142348+adityachandra1@users.noreply.github.com>
2022-07-28 11:26:14 +0530

    lab-2-partial

--- Advanced Programming Lab/Lab-2/Q2_Two_Matrices.py
+++ Advanced Programming Lab/Lab-2/Q2_Two_Matrices.py
@@ -4,6 +4,7 @@ mat1 = {}
 mat2 = {}
 mat3 = {}
 
+#input
 print("Enter Matrix 1 : ")
 for i in range(x):
     row = list(map(int, input().split()))
@@ -16,17 +17,20 @@ for i in range(x):
     for j in range(x):
         mat2[(i,j)] = row[j]
 
-
+#Adding
 for i in range(x):
         for j in range(y):
             mat3[(i,j)] = mat1[(i,j)] + mat2[(i,j)]
 
-print("Dictionary" : mat3);
+print("After Addition, Dictionary: ", mat3)
 
-matrix = [[]]
+matrix = [[0]*x]*y
 for elem in mat3:
-    elem
-    
+    i = elem[0]
+    j = elem[1]
+    matrix[i][j] = mat3[(i,j)]
+
+print ("After Addition, Matrix: " , matrix)
 
 
 
